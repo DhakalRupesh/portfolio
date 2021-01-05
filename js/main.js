6e49993ad121680094418c5f15bdca420317f98a
DhakalRupesh <a73dhakal@outlook.com>
2021-01-05 21:36:26 +0545

    date

--- js/main.js
+++ js/main.js
@@ -1,57 +1,63 @@
 //    <!-- shadow on scroll-->
-{/* <script type="text/javascript"> */}
-   window.addEventListener('scroll', function(){
-       let header = document.querySelector('header');
-       let windowposition =  window.scrollY > 0
-       header.classList.toggle('scrolling-active', windowposition);
-   });
+{
+  /* <script type="text/javascript"> */
+}
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let windowposition = window.scrollY > 0;
+  header.classList.toggle("scrolling-active", windowposition);
+});
 // </script>
 
 // <!-- navlink active -->
-{/* <script type="text/javascript"> */}
-    $(document).on('click', 'ul li', function(){
-        $(this).addClass('active_link').siblings().removeClass('active_link');
-    });
+{
+  /* <script type="text/javascript"> */
+}
+$(document).on("click", "ul li", function () {
+  $(this).addClass("active_link").siblings().removeClass("active_link");
+});
 // </script>
-
 
 // scroll animations---------------------
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 1500,
-    reset: true
+  origin: "top",
+  distance: "80px",
+  duration: 1500,
+  reset: true,
 });
 
 /*SCROLL HOME*/
-sr.reveal('.body-name',{});
-sr.reveal('.greeting',{}); 
-sr.reveal('.body-profession',{}); 
-sr.reveal('.body-list',{delay: 100}); 
-sr.reveal('.profile',{delay: 100}); 
-sr.reveal('.body-list-social',{ interval: 100}); 
+sr.reveal(".body-name", {});
+sr.reveal(".greeting", {});
+sr.reveal(".body-profession", {});
+sr.reveal(".body-list", { delay: 100 });
+sr.reveal(".profile", { delay: 100 });
+sr.reveal(".body-list-social", { interval: 100 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.img-about',{}); 
-sr.reveal('.about-desc',{delay: 100});
-sr.reveal('.about-goodat-container',{delay: 100}); 
-sr.reveal('.about-btn-contactme',{delay: 100}); 
+sr.reveal(".img-about", {});
+sr.reveal(".about-desc", { delay: 100 });
+sr.reveal(".about-goodat-container", { delay: 100 });
+sr.reveal(".about-btn-contactme", { delay: 100 });
 
 // what i do
-sr.reveal('.img-about',{}); 
-sr.reveal('.about-icon i',{delay: 100}); 
-sr.reveal('.about-card h3',{delay: 100});
-sr.reveal('.about-card p',{delay: 100}); 
+sr.reveal(".img-about", {});
+sr.reveal(".about-icon i", { delay: 100 });
+sr.reveal(".about-card h3", { delay: 100 });
+sr.reveal(".about-card p", { delay: 100 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.bar',{interval: 100}); 
-sr.reveal('.img-skill-circle',{delay: 100});
+sr.reveal(".bar", { interval: 100 });
+sr.reveal(".img-skill-circle", { delay: 100 });
 
 // contact
-sr.reveal('.copyright', {delay: 100});
-sr.reveal('.contact-icon i',{delay: 100}); 
-sr.reveal('.contact-card h4',{delay: 100});
-sr.reveal('.contact-card p',{delay: 100}); 
+sr.reveal(".copyright", { delay: 100 });
+sr.reveal(".contact-icon i", { delay: 100 });
+sr.reveal(".contact-card h4", { delay: 100 });
+sr.reveal(".contact-card p", { delay: 100 });
 
 // education
-sr.reveal('.edu-quilification', {delay: 100});
+sr.reveal(".edu-quilification", { delay: 100 });
+
+// date
+var today = new Date() var year = today.getFullYear() document.write(year)
